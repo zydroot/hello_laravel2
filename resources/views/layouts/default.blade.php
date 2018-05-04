a6272aa82b4adbd48cda9e4dd4ee8e89599da0ff
zydroot <395724328@qq.com>
2018-05-04 08:22:03 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,16 @@
 <html>
 <head>
     <title>@yield('title','laravel-zyd')</title>
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+</div>
+
 </body>
 </html>
